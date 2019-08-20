13c28ebdfaa8fe4423994567d8af8c5589f4e1d2
AnnaBorovaya <anna.kviten11@gmail.com>
2019-08-20 13:52:25 +0300

    some

---
+++ precache-manifest.a0772c2bbc23522ca77836b57fa44d22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b7ee922fb7fbf8d2fa32b6410e3e8c",
+    "url": "/book_store-react/index.html"
+  },
+  {
+    "revision": "0063789e45c7c8c2d3d5",
+    "url": "/book_store-react/static/css/main.03276bd4.chunk.css"
+  },
+  {
+    "revision": "39b701946748130cba6c",
+    "url": "/book_store-react/static/js/2.ca2e6682.chunk.js"
+  },
+  {
+    "revision": "0063789e45c7c8c2d3d5",
+    "url": "/book_store-react/static/js/main.2ca2691f.chunk.js"
+  },
+  {
+    "revision": "4b1be7df1b17e7d5908f",
+    "url": "/book_store-react/static/js/runtime~main.b0332a9c.js"
+  }
+]);
